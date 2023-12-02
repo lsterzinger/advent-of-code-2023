97f6579d1609233082449a901b73e3ded3d6e2da
Lucas Sterzinger <lucassterzinger@gmail.com>
2023-12-02 09:24:36 -0800

    Squashed commit of the following:
    commit 52704dce512c09c7bd7a2d99f4299dd93a1944db
    Author: Lucas Sterzinger <lucassterzinger@gmail.com>
    Date:   Sat Dec 2 09:23:14 2023 -0800
    
        Finished 1

--- 01/01.py
+++ 01/01.py
@@ -1,28 +1,59 @@
-part2 = True
+part2 = False
+input_path = './01-input.txt'
 
-numbers = {
-    'one' : 1,
-    'two' : 2,
-    'three' : 3,
-    'four' : 4,
-    'five' : 5,
-    'six' : 6,
-    'seven' : 7,
-    'eight' : 8,
-    'nine' : 9,
+# Translation dictionary of natural language numbers to string digit
+number_dict = {
+    'one' : '1',
+    'two' : '2',
+    'three' : '3',
+    'four' : '4',
+    'five' : '5',
+    'six' : '6',
+    'seven' : '7',
+    'eight' : '8',
+    'nine' : '9'
 }
 
-with open('./01-sample.txt') as input:
-    total = 0
-    for l in input.readlines():
-        if part2:
-            # If doing part to, replace the words of digits with the digits themselves
-            l2 = l
-            for t,i in numbers.items():
-                l = l.replace(t, str(i))
-            print(l, l2)
-        digits = [s for s in l if s.isdigit()]
+
+# Start at the left, return if digit
+def find_left_number(l, part2=False):
+    i = 0
+    while i < len(l):
+        if l[i].isdigit(): 
+            return l[i]
+
+        # If doing part to, return digit if first `i` characters 
+        # match a natural language number
+        elif part2:
+            for n in number_dict.keys():
+                if n in l[:i+1]:
+                    return number_dict[n]
+        i += 1 
+    return None
+
+# Start at right, return if digit
+def find_right_number(l, part2=False):
+    i = len(l)-1
+    while i >= 0:
+        if l[i].isdigit(): return l[i]
         
-        number = int(digits[0] + digits[-1])
-        total += number
-    print(total)
+        # If doing part 2, return digit if characters from i -> end 
+        # match a natural language number
+        elif part2:
+            for n in number_dict.keys():
+                if n in l[i:]:
+                    return number_dict[n]
+        i -= 1
+    return None
+
+with open(input_path) as f:
+    lines = f.read().splitlines()
+
+total = 0
+for l in lines:
+    d1 = find_left_number(l, part2)
+    d2 = find_right_number(l, part2)
+
+    number = int(d1 + d2)
+    total += number
+print(total)
